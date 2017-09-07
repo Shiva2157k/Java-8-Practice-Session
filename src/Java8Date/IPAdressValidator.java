4dde69fb6afb72f3730dd966e78d49625b337fa6
shiva2157k <shivak916@gmail.com>
2017-09-06 21:32:45 -0400

    feat(app-IPAddressChecker)

---
+++ src/Java8Date/IPAdressValidator.java
@@ -0,0 +1,28 @@
+package Java8Date;
+
+import java.util.Scanner;
+
+public class IPAdressValidator {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+class MyRegex{
+ 
+    String pattern;
+
+    MyRegex(){
+        pattern = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+    }
+
+
+
+}
