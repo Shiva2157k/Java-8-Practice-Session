365d728c6031b7d6d80bd1e132638e8dd91069e4
shiva2157k <shivak916@gmail.com>
2017-09-08 10:46:32 -0400

    feat(app-maxOccurences)

---
+++ src/Java8Date/MaximumOccurences.java
@@ -0,0 +1,33 @@
+package Java8Date;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaximumOccurences {
+
+    public static void main(String args[]){
+
+        String[] words = {"Tom","Gopal","Jack","Tom","Jerry","Gopal","Bob","Gopal","Jack","Hari","Gopal"};
+                                                            
+        Map<String,Integer> occurences = new  HashMap<String,Integer>();
+
+        for (String w: words
+             ) {
+                Integer temp = occurences.get(w);
+                occurences.put(w,(temp == null)?1:temp+1);
+        }
+        int max = 0;
+        String ans = null;
+        for(Map.Entry<String, Integer> m : occurences.entrySet()){
+
+            if(m.getValue() > max){
+                max = m.getValue();
+                ans = m.getKey();
+
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+}
